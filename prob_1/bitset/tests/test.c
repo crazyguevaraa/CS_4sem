8e5d79c8796d3f1450664c7f454a74ff48d5b753
Danila Puchkov <crazyguevara1488@gmail.com>
2022-05-04 07:53:48 +0300

    make new test

--- prob_1/bitset/tests/test.c
+++ prob_1/bitset/tests/test.c
@@ -14,18 +14,46 @@ int run_all (){
     return 0;
 }
 
-void test_create (){
+void test_err (){
+    bitset_t* bitset = bit_ctor (128);
+
+    assert (bitset != NULL);
+    assert (bit_set_all (bitset) == OK);
+    assert (bit_unset_all (bitset) == OK);
+    assert (bit_set_val_all (bitset, -1) == INV_VAL);
+    assert (bit_set_val_all (bitset, SET) == OK);
+    assert (bit_check_set_all (bitset) == 1);
+    assert (bit_set_val_all (bitset, UNSET) == OK);
+    assert (bit_check_set_all (bitset) == 0);
+    assert (bit_check_set_any (bitset) == 0);
+    assert (bit_rvs (bitset) == OK);
+    assert (bit_check_set_any (bitset) == 1); 
+    assert (bit_set (bitset, -128) == INV_POS);
+    assert (bit_set (bitset, 129) == INV_POS);
+    assert (bit_unset (bitset, 64) == OK);
+    assert (bit_unset (bitset, -128) == INV_POS);
+    assert (bit_unset (bitset, 129) == INV_POS);
+    assert (bit_set_val (bitset, 64, UNSET) == OK);
+    assert (bit_set_val (bitset, -1, SET) == INV_POS);
+    assert (bit_set_val (bitset, 129, SET) == INV_POS);
+    assert (bit_set_val (bitset, 64, -1) == INV_VAL);
+    assert (bit_get (bitset, -1) == INV_POS);
+    assert (bit_get (bitset, 164) == INV_POS); 
+
+    assert (bit_dtor (bitset) == OK);
+    
     
 }
 
-void test_err (){
-    unsigned long cap = 128;
-    bitset_t* bitset = bit_ctor (cap);
-    assert (bitset != NULL);
-    assert (bit_cap (bitset) < cap);
-    assert (bit_dtor (bitset) != OK);
-    assert (bit_dtor (NULL) != INV_SET);
-    
+void test_create (){
+    bitset_t* bitset = bit_ctor (0);
+    assert (bitset == NULL);
+    assert (bit_dtor (bitset) == INV_SET);
+    bitset = bit_ctor (128);
+    assert (bit_cap (bitset) == 128);
+    assert (bit_dtor (bitset) != INV_ARR);
+
+
 
 
 }
